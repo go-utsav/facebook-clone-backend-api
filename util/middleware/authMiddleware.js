5a421cfa98988cc6dde8b02b0c712a39484b794e
go-utsav <utsavgohel3@gmail.com>
2023-03-03 18:14:54 +0530

    token fix

--- util/middleware/authMiddleware.js
+++ util/middleware/authMiddleware.js
@@ -2,31 +2,39 @@
 const jwt = require("jsonwebtoken");
 // todo if no token pASSED HOULD RETURN UNAAUTHORIZED  MESSAGE
 const auth = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
+  try {
+    const tokenn = req.headers.authorization;
 
-        const verifyToken = jwt.verify(
-            token.toString(),
-            process.env.ACCESS_KEY_TOKEN
-        );
+    if (!tokenn) {
+      return res.json({
+        status: "error",
+        message: "Access token Missing",
+      });
+    } else {
+      const token = req.headers.authorization.split(" ")[1];
+      const verifyToken = jwt.verify(
+        token.toString(),
+        process.env.ACCESS_KEY_TOKEN
+      );
 
-        if (verifyToken) {
-            req.user_id = verifyToken.id;
-            req.name = verifyToken.firstname + " " + verifyToken.lastname;
-            next();
-        } else {
-            return res.json({
-                status: "error",
-                message: "Unauthorized access",
-            });
-        }
-    } catch (err) {
-        console.error(err);
+      if (verifyToken) {
+        req.user_id = verifyToken.id;
+        req.name = verifyToken.firstname + " " + verifyToken.lastname;
+        next();
+      } else {
         return res.json({
-            status: "error",
-            message: "internal server error ",
+          status: "error",
+          message: "Unauthorized access",
         });
+      }
     }
+  } catch (err) {
+    console.error(err);
+    return res.json({
+      status: "error",
+      message: "Intenal server error: ",
+    });
+  }
 };
 
 module.exports = auth;
